0fc5cb8d71889bfb4a0728647f562952754e0038
PAVAN-WS\Marco Pavan <m.pavan@neticon.it>
2018-05-10 11:47:55 +0200

    Picola pulizia e fix

--- test/unit/specs/Userslist.spec.js
+++ test/unit/specs/Userslist.spec.js
@@ -1,5 +1,3 @@
-// Import Vue and the component being tested
-import Vue from 'vue'
 import UsersList from '../../../src/components/UsersList'
 
 describe('UsersList', () => {
@@ -16,11 +14,3 @@ it('sets the correct default data', () => {
   expect(defaultData.usersLoaded).to.equal(false)
   expect(defaultData.usersUrl).to.equal('https://api.github.com/users/octocat/followers')
 })
-
-// Mount an instance and inspect the render output
-it('renders the correct message', () => {
-  const Constructor = Vue.extend(UsersList)
-  const vm = new Constructor().$mount()
-  expect(vm.$el.textContent.trim()).to.equal('La top 10 dei partecipanti. Sei tra loro?')
-})
-
